2edcb317073ce8795b53c660a1f9ff89f2c6a6ba
Rajan GC <57903373+gcrajan@users.noreply.github.com>
2023-07-03 03:38:52 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+import pageStyle from '../styles/page.module.css'
+import Header from '../components/Header'
+import HomeMain from '../components/HomeMain'
+import MusicList from '../components/MusicList'
+import Footer from '../components/Footer'
+
+
 export default function Home() {
   return (
-    <div>
-      <h1>Music Sansar</h1>
-      <p>Sansar for music lovers</p>
+    <div className={pageStyle.homeContainer}>
+      <Header/>
+      <HomeMain/>
+      <div className={pageStyle.pageDiv}>
+        <p>Artist List</p>
+        <h1>Some of the list of artist are:</h1>
+      </div>
+      <MusicList/>
+      <Footer/>
     </div>
   );
 }
