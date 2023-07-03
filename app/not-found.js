c1f98a0a04d3337429b2267bd43562d3d4b86e3f
Rajan GC <57903373+gcrajan@users.noreply.github.com>
2023-07-03 12:26:20 +0545

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+import Logo from '../images/error.png'
+import errorStyle from '../styles/error.module.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export default function NotFound(){
     return(
-        <div>
-            <h1>This page is not available</h1>
+        <div className={errorStyle.errorDiv}>
+            <Header/>
+            <div className={errorStyle.errorDivImg}>
+                <Image src={Logo} alt="logo"/>
+            </div>
+            <Footer/>
         </div>
     )
 }
