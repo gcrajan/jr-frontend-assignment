bf8cdeea051f33e2b47fe6c001b0cc61f0111062
Rajan GC <57903373+gcrajan@users.noreply.github.com>
2023-07-03 15:54:53 +0545

    component

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from 'next/image'
+import Logo from '../images/logo.png'
+import React from 'react'
+import headerStyle from '../styles/header.module.css'
+
+const Header = () => {
+  return (
+    <nav className={headerStyle.navbar}>
+        <div className={headerStyle.imageNav}>
+            <Link href='/'> <Image src={Logo} alt="logo"/> </Link>
+        </div>
+        <div className={headerStyle.contentNav}>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/search">Search</Link>
+                </li>
+                <li>
+                    <Link href="/about">About us</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
